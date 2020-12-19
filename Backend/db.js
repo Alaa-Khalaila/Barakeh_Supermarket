8481e5b08ddf90138b93bb8a058d426a4460c8a3
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-19 19:41:46 +0200

    connected the database

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+const mysql2 = require('mysql2');
+
+const db = mysql2.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log("db error :", err.stack);
+        return;
+    }
+    console.log("db ID :", db.threadId)
+});
+
+module.exports = db
