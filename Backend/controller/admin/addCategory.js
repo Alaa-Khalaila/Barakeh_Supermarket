55645207cca7527ad61c8c186f39556802475edb
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-20 13:25:01 +0200

    finished add category api

--- Backend/controller/admin/addCategory.js
+++ Backend/controller/admin/addCategory.js
@@ -1,7 +1,13 @@
-const addCategory = (req, res) => {
-    const {name,img_url} = req.body
+const connection = require("../../db")
 
-    res.json("hello")
+const addCategory = (req, res) => {
+    const { name, img_url } = req.body
+    const query = `INSERT INTO category (name,img_url) VALUES (?,?)`;
+    const data = [name,img_url];
+    connection.query(query, data, (err, result) => {
+        if (err) throw err
+    });
+    res.json("Added category");
 }
 
 module.exports = addCategory;
